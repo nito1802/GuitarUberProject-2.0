a22b85f6a6e94902e24a68187df898d8af19cbbb
Jarek Szczupak <dante1802@hotmail.com>
2024-06-01 12:55:10 +0200

    namespace fix

--- AudioMaker.Interfaces/Models/MidiExport/PlaylistKlocekChordModel.cs
+++ AudioMaker.Interfaces/Models/MidiExport/PlaylistKlocekChordModel.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AudioMaker.Interfaces.Models.MidiExport
+﻿namespace AudioMaker.Interfaces.Models.MidiExport
 {
     public class PlaylistKlocekChordModel
     {
